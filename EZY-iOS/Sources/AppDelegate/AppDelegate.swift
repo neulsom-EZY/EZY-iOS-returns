74a6fbe19748742729ba780a358b2ccdabbca485
JiHoonAHN <ahnjh1028@naver.com>
2022-05-03 13:26:40 +0900

    [CREATE] Setting

---
+++ EZY-iOS/Sources/AppDelegate/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  EZY-iOS
+//
+//  Created by Ji-hoon Ahn on 2022/05/03.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
---
+++ EZY-iOS/Sources/AppDelegate/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  EZY-iOS
+//
+//  Created by Ji-hoon Ahn on 2022/05/03.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let navController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+    }
+
+}
+
