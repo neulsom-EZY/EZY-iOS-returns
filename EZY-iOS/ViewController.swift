2b19e0ad15064b2c562690181c3f4a63a4eaed2c
JiHoonAHN <ahnjh1028@naver.com>
2022-05-03 12:48:59 +0900

    Initial Commit

---
+++ EZY-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EZY-iOS
+//
+//  Created by Ji-hoon Ahn on 2022/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
